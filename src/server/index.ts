9476b9932e55aab8629b0910489db47fa512c5e4
Neal Fennimore <hi@neal.codes>
2023-01-24 11:58:05 -0500

    Add in CORS handling

--- src/server/index.ts
+++ src/server/index.ts
@@ -20,6 +20,47 @@ const requiresSession = (request: IRequest, env: Env) => {
     }
 };
 
+const corsHeaders = {
+    'Access-Control-Allow-Origin': 'passkeys.neal.codes',
+    'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+    'Access-Control-Max-Age': '86400',
+};
+
+router.options('*', function handleOptions(request) {
+    // Make sure the necessary headers are present
+    // for this to be a valid pre-flight request
+    let headers = request.headers;
+    if (
+        headers.get('Origin') !== null &&
+        headers.get('Access-Control-Request-Method') !== null &&
+        headers.get('Access-Control-Request-Headers') !== null
+    ) {
+        // Handle CORS pre-flight request.
+        // If you want to check or reject the requested method + headers
+        // you can do that here.
+        let respHeaders = {
+            ...corsHeaders,
+            // Allow all future content Request headers to go back to browser
+            // such as Authorization (Bearer) or X-Client-Name-Version
+            'Access-Control-Allow-Headers': request.headers.get(
+                'Access-Control-Request-Headers'
+            ),
+        };
+
+        return new Response(null, {
+            headers: respHeaders,
+        });
+    } else {
+        // Handle standard OPTIONS request.
+        // If you want to allow other HTTP Methods, you can do that here.
+        return new Response(null, {
+            headers: {
+                Allow: 'GET, HEAD, POST, OPTIONS',
+            },
+        });
+    }
+});
+
 router.post('*', withContext);
 router.post('/attestation/generate', async (request) => {
     try {
