475723a201f09944b8a1c6dd7a779bc699633a53
Neal Fennimore <hi@neal.codes>
2023-01-25 13:04:44 -0500

    Fix overriding button bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/client/**/*.{html,js}'],
+    content: ['./src/client/assets/*.{html,js}'],
     theme: {
         extend: {},
     },
