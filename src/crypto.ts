e7c06c5b490734ae0f38931fa97a6112ff01a696
Neal Fennimore <hi@neal.codes>
2023-01-25 09:27:08 -0500

    Update to try raw imports

--- src/crypto.ts
+++ src/crypto.ts
@@ -48,7 +48,7 @@ export class Crypto {
         namedCurve: SigningCurve
     ) {
         return await crypto.subtle.importKey(
-            'spki',
+            'raw',
             pubKey,
             { name, namedCurve },
             true,
