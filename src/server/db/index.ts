568102261c3addd1ef6e71838df86b4c865c2ff4
Neal Fennimore <hi@neal.codes>
2023-01-25 16:52:55 -0500

    Remove bad insert for cosealg

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -50,7 +50,7 @@ export class DB {
         const { kid, pubkey, attestationObject } = payload;
 
         return this.D1.prepare(
-            'INSERT INTO public_keys(kid, pubkey, attestation_data, cose_alg, user_id) VALUES(?1, ?2, ?3, ?4, ?5)'
+            'INSERT INTO public_keys(kid, pubkey, attestation_data, user_id) VALUES(?1, ?2, ?3, ?4)'
         ).bind(
             kid,
             safeByteEncode(pubkey),
