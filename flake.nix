5b285ca05530dfc6917ddc2fba6445b67bcbef31
Neal Fennimore <neal@neal.codes>
2026-06-22 16:09:14 -0400

    Add in flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Passkeys example dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.nodejs ];
+        };
+      });
+    };
+}
